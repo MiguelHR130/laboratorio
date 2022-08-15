d80990edb3bee2891d41d9d958ac1e1863e9790e
Angel Hernandez <miguel.hernandez@conservct.com>
2022-08-15 12:31:45 -0500

    todo

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import '../sass/app.scss' 
+import * as bootstrap from 'bootstrap' 
+import '../assets/plantilla/css/font-awesome.min.css'
+import '../assets/plantilla/css/simple-line-icons.min.css'
+import '../assets/plantilla/css/style.css'
+import '../assets/plantilla/js/jquery.min.js'
+import '../assets/plantilla/js/popper.min.js'
+import '../assets/plantilla/js/bootstrap.min.js'
+import '../assets/plantilla/js/pace.min.js'
+import '../assets/plantilla/js/Chart.min.js'
+import '../assets/plantilla/js/template.js'
+
+ 
